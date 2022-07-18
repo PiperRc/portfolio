2248a74358ad3ee231b09af7d0d0a67c6b900d39
Cheyenne Piper <72780256+PiperRc@users.noreply.github.com>
2022-07-17 21:45:55 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // alert('fix project so that grid does not control whole project,side navbar,etc')
+
+// alert('fix width of main when on desktop')
+const hamburger=document.querySelector('.hamburger');
+const navbar=document.querySelector('.navbar');
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('hamburger-click');
+    navbar.classList.toggle('navbar-display')
+})
