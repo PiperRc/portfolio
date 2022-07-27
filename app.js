4fbf86cb1b5bdd9499024e472ed3774b2e9c2e31
Cheyenne Piper <72780256+PiperRc@users.noreply.github.com>
2022-07-27 16:59:43 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3,21 +3,26 @@
 // alert('fix width of main when on desktop')
 const hamburger=document.querySelector('.hamburger');
 const navbar=document.querySelector('.navbar');
-const body=document.querySelector('body');
 const main=document.querySelector('main');
+const navbarNav=document.querySelector('.navbar-nav')
 
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('hamburger-click');
     navbar.classList.toggle('navbar-display');
-    
+    navbarNav.classList.toggle('navbar-nav-display');
   
 })
+
+
 
 main.addEventListener('click',()=>{
     if (navbar.classList.contains('navbar-display')){
         navbar.classList.remove('navbar-display')
     }
+    if(navbarNav.classList.contains('navbar-nav-display')){
+        navbarNav.classList.remove('navbar-nav-display');
+    }
     hamburger.classList.remove('hamburger-click');
-
 })
+
 
